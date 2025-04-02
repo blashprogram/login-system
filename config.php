d5150f0043308f8f02229300d5f67e3671a3d20d
blashprogram <tangirahmed0@gmail.com>
2025-04-02 19:56:47 +0600

    a

--- config.php
+++ config.php
@@ -5,9 +5,11 @@ $username = "tangir";
 $password = "C42bkoHPWYYeAupVdXwWA4gmK4ycUbOT";
 $dbname = "login_0y6d";
 
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+try {
+    $conn = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Database Connected!";
+} catch (PDOException $e) {
+    die("Database Connection Failed: " . $e->getMessage());
 }
 ?>
